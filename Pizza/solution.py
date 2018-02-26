11b045c5deb05f1d8b3fdc80106a6f41fd409417
Marco Cieno <cieno.marco@gmail.com>
2018-02-26 01:04:44 +0100

    Pizza - faster algorithm

--- Pizza/solution.py
+++ Pizza/solution.py
@@ -2,7 +2,7 @@ import sys
 
 
 def sizes(num):
-    """Metodo generatore di possibili coppie di numeri interi che formano un
+    """Genera tutte le possibili coppie di numeri interi che formano un
     rettangolo di una certa area.
 
     Positional arguments:
@@ -21,7 +21,7 @@ def sizes(num):
         yield (i, num // i)
 
 
-def mask(mat, max_r, max_c, count, coord, combs, ignore):
+def mask(mat, max_r, max_c, count, row, col, combs, ignore):
     """Testa tutte le possibili dimensioni di fetta e ritorna quella che
     soddisfa le condizioni sugli ingredienti, aggiornando di conseguenza
     l'insieme di spicchi gia' impegnati.
@@ -31,24 +31,25 @@ def mask(mat, max_r, max_c, count, coord, combs, ignore):
     max_r -- righe (altezza) della pizza.
     max_c -- colonne (larghezza) della pizza.
     count -- numero minimo di ciascun ingrediente che la fetta deve contenere.
-    coord -- cordinata alla quale la fetta inizia (angolo alto sinistro).
+    row -- indice di riga alla quale la fetta inizia (angolo alto sinistro).
+    col -- indice di colonna alla quale la fetta inizia (angolo alto sinistro).
     combs -- lista di possibili combinazioni di dimensioni della fetta.
-    ignore -- insieme di coordinate degli spicchi gia' impegnati in altre fette.
+    ignore -- matrice booleana tale che ignore[i][j] == True se e solo se la
+              cella (i, j) della pizza e' gia' impegnata in un'altra fetta.
 
     Returns:
     Combinazione di altezza-larghezza tra quelle presenti in combs
     che individua una fetta con i requisiti richiesti sugli ingredienti.
     Se nessuna combinazione e' soddisfacente, ritorna None.
     """
-    row, col = coord
     # Testa ogni combinazione possibile di dimensione della fetta
     for comb in combs:
         h, w = comb
         # Flag che diventa True se la fetta contiene un pezzo gia' contenuto in
         # un'altra fetta
         abort = False
-        # Insieme delle coordinate che la fetta attuale occupa
-        hovering = set()
+        # Lista delle coordinate che la fetta attuale occupa
+        hovering = []
         # Contatori del numero di ingredienti nella fetta
         tomatoes = 0
         mushrooms = 0
@@ -61,19 +62,25 @@ def mask(mat, max_r, max_c, count, coord, combs, ignore):
         # prossima combinazione
         for r in range(row, row + h):
             for c in range(col, col + w):
-                if (r, c) in ignore:
+                if ignore[r][c] is True:
                     abort = True
                     break
-                hovering.add((r, c))
+                hovering.append((r, c))
                 if mat[r][c] == 'T':
                     tomatoes += 1
                 else:
                     mushrooms += 1
             if abort is True:
                 break
-        # Se la fetta soddisfa i requisiti, ritorna la combinazione
-        if abort is not True and tomatoes >= count and mushrooms >= count:
-            ignore.update(hovering)
+        # Se la combinazione non andava bene, passa alla prossima
+        if abort is True:
+            continue
+        # La fetta soddisfa i requisiti
+        if tomatoes >= count and mushrooms >= count:
+            # Aggiorna la matrice dei pezzi da ignorare
+            for y, x in hovering:
+                ignore[y][x] = True
+            # Ritorna la combinazione altezza-larghezza trovata
             return comb
     # Non e' stata trovata alcuna fetta
     return None
@@ -88,7 +95,8 @@ def grow(s, limit, max_r, max_c, ignore):
     limit -- numero massimo di spicchi che la fetta puo' contenere.
     max_r -- righe (altezza) della pizza.
     max_c -- colonne (larghezza) della pizza.
-    ignore -- insieme di coordinate degli spicchi gia' impegnati in altre fette.
+    ignore -- matrice booleana tale che ignore[i][j] == True se e solo se la
+              cella (i, j) della pizza e' gia' impegnata in un'altra fetta.
 
     Returns:
     Fetta di pizza appena allargata.
@@ -96,32 +104,30 @@ def grow(s, limit, max_r, max_c, ignore):
     # Espandi, finche' possibile, la fetta verso l'alto
     while s['idx'][0] > 0 and \
           (s['siz'][0] + 1) * s['siz'][1] <= limit and \
-          all((s['idx'][0] - 1, s['idx'][1] + k) not in ignore for k in range(s['siz'][1])):
+          all(ignore[s['idx'][0] - 1][s['idx'][1] + k] is not True for k in range(s['siz'][1])):
         s['idx'] = (s['idx'][0] - 1, s['idx'][1])
         s['siz'] = (s['siz'][0] + 1, s['siz'][1])
     # Espandi, finche' possibile, la fetta verso sinistra
     while s['idx'][1] > 0 and \
           s['siz'][0] * (s['siz'][1] + 1) <= limit and \
-          all((s['idx'][0] + k, s['idx'][1] - 1) not in ignore for k in range(s['siz'][0])):
+          all(ignore[s['idx'][0] + k][s['idx'][1] - 1] is not True for k in range(s['siz'][0])):
         s['idx'] = (s['idx'][0], s['idx'][1] - 1)
         s['siz'] = (s['siz'][0], s['siz'][1] + 1)
     # Espandi, finche' possibile, la fetta verso il basso
     while s['idx'][0] + s['siz'][0] < max_r and \
           (s['siz'][0] + 1) * s['siz'][1] <= limit and \
-          all((s['idx'][0] + s['siz'][0], s['idx'][1] + k) not in ignore for k in range(s['siz'][1])):
+          all(ignore[s['idx'][0] + s['siz'][0]][s['idx'][1] + k] is not True for k in range(s['siz'][1])):
         s['siz'] = (s['siz'][0] + 1, s['siz'][1])
     # Espandi, finche' possibile, la fetta verso destra
     while s['idx'][1] + s['siz'][1] < max_c and \
           s['siz'][0] * (s['siz'][1] + 1) <= limit and \
-          all((s['idx'][0] + k, s['idx'][1] + s['siz'][1]) not in ignore for k in range(s['siz'][0])):
+          all(ignore[s['idx'][0] + k][s['idx'][1] + s['siz'][1]] is not True for k in range(s['siz'][0])):
         s['siz'] = (s['siz'][0], s['siz'][1] + 1)
 
     # Aggiorna le coordinate da ignorare
-    hovering = set()
     for i in range(s['siz'][0]):
         for j in range(s['siz'][1]):
-            hovering.add((s['idx'][0] + i, s['idx'][1] + j))
-    ignore.update(hovering)
+            ignore[s['idx'][0] + i][s['idx'][1] + j] = True
 
     return s
 
@@ -159,7 +165,7 @@ if __name__ == '__main__':
     # Il nome del file viene letto come argomento da riga di comando
     if len(sys.argv) < 2:
         sys.stderr.write('Filename missing\n')
-        quit()
+        sys.exit()
 
     with open(sys.argv[1], 'r') as f:
         pizza = [x.strip() for x in f]
@@ -190,10 +196,10 @@ if __name__ == '__main__':
         sys.stdout.write('-' * 30 + '\n')
     # DEBUG -- END
 
-    # Inizializza la lista delle fette (slices) e l'insieme delle coordinate
-    # gia' occupate, e dunque da ignorare (already).
+    # Inizializza la lista delle fette (slices) e una matrice ausiliaria per
+    # tenere memoria delle coordinate gia' occupate (already).
     slices = []
-    already = set()
+    already = [[False] * cols for _ in range(rows)]
 
     # Per ogni pezzetto di pizza, cerca se esiste una fetta che abbia questo
     # elemento nell'angolo in alto a sinistra e soddisfa le condizioni richieste
@@ -208,7 +214,7 @@ if __name__ == '__main__':
             # una certa dimensione. Cerca per ognuna di queste se esiste una
             # fetta valida per quanto riguarda il numero minimo di ingredienti
             for combs in all_combs:
-                x = mask(pizza, rows, cols, min_pieces, (i, j), combs, already)
+                x = mask(pizza, rows, cols, min_pieces, i, j, combs, already)
                 if x is not None:
                     # DEBUG -- START
                     if DEBUG is True:
@@ -221,17 +227,17 @@ if __name__ == '__main__':
                     #  - 'siz': coppia (h, w) dell'altezza e larghezza della
                     #           fetta
                     slices.append({'idx': (i, j), 'siz': x})
-                    # Avendo appena aggiunto una fetta, saltiamo abbastanza
-                    # colonne, evitando cicli inutili.
-                    # Nota: vanno saltate x[1] caselle, dato che fuori da questo
-                    #       ciclo viene aggiunto 1 in ogni caso, ne fa saltare
-                    #       x[1] - 1, e poi viene riaggiunta l'unita'.
-                    j += x[1] - 1
                     # Rompi il for, in quanto non serve controllare altre
                     # combinazioni, dato che l'abbiamo gia' trovata quella buona
                     break
-            # I cicli infiniti non ci piacciono
-            j += 1
+            # Mediamente l'indice di colonna potra' essere incrementato di
+            # quantita' maggiori di 1, questo perche' buona parte della pizza e'
+            # gia' occupata da fette. In questo modo le iterazioni saranno meno
+            if already[i][j] is not True:
+                j += 1
+            else:
+                while j < cols and already[i][j] is True:
+                    j += 1
 
     # Porva a espandere tutte le fette
     for i in range(len(slices)):
@@ -257,7 +263,7 @@ if __name__ == '__main__':
         sys.stdout.write('{} PIZZA AFTER SLICING {}\n'.format('-' * 10, '-' * 10))
         for row in range(rows):
             for col in range(cols):
-                if (row, col) in already:
+                if already[row][col] is True:
                     sys.stdout.write('*')
                 else:
                     sys.stdout.write(pizza[row][col])
